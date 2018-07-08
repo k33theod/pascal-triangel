4ecbdd53e106915892696877c5c87bc6449547c2
k33theod <k33theod@gmail.com>
2018-07-08 22:05:00 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
 #include<math.h>
-
 double  factorial (int n);
 double  factorial2 (int n);
 double bin_coefficiend (int n, int r);
-double bin_theorem(int a, int b, int n);
 void pascal_traingle(int n);
 
 int main(int argc, char **argv){
-	int n =5;
-    printf("Το %d  παραγοντικό είναι %g\n",n,factorial2(n));
-    printf("binomial coefficient of (9, 4) is %g \n", bin_coefficiend(9,4));
-    printf(" (7 + 8)**5  is %g \n", bin_theorem(7,8,5));
     pascal_traingle(20);
 	return 0;
 }
@@ -35,23 +29,8 @@ double  factorial2 (int n){
 }
 
 double bin_coefficiend (int n, int r){
-   // if (n<=r)
-      //  return -1;
     return  factorial(n)/(factorial(r)*factorial(n-r));
 }
-
-double bin_theorem(int a, int b, int n){
-    int r = 0;
-    double result = 0;
-    int n2=n;
-    while (r<=n){
-        result+=bin_coefficiend(n,r)*pow((double)a,n2)*pow((double)b,r);
-        r++;
-        n2--;
-    }
-    return result;
-}
-
 void pascal_traingle(int n){
     int spaces =82;
     int lines=n;
